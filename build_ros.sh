a6d251ce324c41ba39389e1c7ec60cfa48b8619c
jike5 <2719825969@qq.com>
2022-10-12 11:02:47 +0800

    update build_ros

--- build_ros.sh
+++ build_ros.sh
@@ -1,7 +1,7 @@
 echo "Building ROS nodes"
-
-cd Examples/ROS/ORB_SLAM2
-mkdir build
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/workspace/edgeslam/Examples/ROS
+cd Examples/ROS/Edge_SLAM
+rm -rf build && mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
 make -j
